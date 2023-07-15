108a582307fee2e8571268d547a3ee700005958d
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-07-16 06:38:31 +0800

    added form functionality

--- id-card/dduran19/js/scripts.js
+++ id-card/dduran19/js/scripts.js
@@ -1,6 +1,8 @@
 
 const actualId=document.getElementById("actualId")
 const newDetailsForm = document.getElementById("newDetails");
+const submitButton = document.getElementById("submitButton")
+const cancelButton = document.getElementById("cancelButton")
 
 function toggleDetailsForm() {
     try{
@@ -20,5 +22,36 @@ function toggleDetailsForm() {
     }
 }
 
-document.getElementById("actualId").addEventListener("click", toggleDetailsForm);
-document.getElementById("cancelButton").addEventListener("click",toggleDetailsForm)
+function handleSubmit(){
+    const firstName = document.getElementById("firstName").value
+    const middleName = document.getElementById("middleName").value
+    const lastName = document.getElementById("lastName").value
+    const fullName = `${lastName}, ${firstName} ${middleName}`.toUpperCase()
+    const dob = document.getElementById("dob").value
+    const date = new Date(dob)
+    const dateString = [
+                date.getMonth() + 1,
+                date.getDate(),
+                date.getFullYear()].join('/')
+        
+    const sex = document.getElementById("sex").value.toUpperCase()
+    const pob = document.getElementById("pob").value.toUpperCase()
+    const address = document.getElementById("address").value.toUpperCase()
+    const bloodType = document.getElementById("bloodType").value.toUpperCase()
+    const eyesColor = document.getElementById("eyeColor").value.toUpperCase()
+    
+    // console.log(fullName,dateString,sex,pob,address,bloodType,eyesColor)
+    document.getElementById("nameView").innerHTML=fullName
+    document.getElementById("dobView").innerHTML=dateString
+    document.getElementById("sexView").innerHTML=sex
+    document.getElementById("pobView").innerHTML=pob
+    document.getElementById("addressView").innerHTML=address
+    document.getElementById("bloodTypeView").innerHTML=bloodType
+    document.getElementById("eyeColorView").innerHTML=eyesColor
+
+    toggleDetailsForm()
+}
+
+actualId.addEventListener("click", toggleDetailsForm);
+cancelButton.addEventListener("click",toggleDetailsForm)
+submitButton.addEventListener("click",handleSubmit)
