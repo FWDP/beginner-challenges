fb47d501acee152907395f8a38f5f70aca7ab075
toney0103 <toney010319@gmail.com>
2023-07-18 23:15:03 +0800

    Updating Form and Adding some Images

--- id-card/d4ydr34mer/index.js
+++ id-card/d4ydr34mer/index.js
@@ -1,35 +1,49 @@
-let inputName = document.getElementById("full-name")
-let fullName = document.getElementById("fullname")
+//  NAME VARIABLES
+ let defSName = document.getElementById("surename")
+ let InputSname=  document.getElementById("input-surename")
+ let defFname = document.getElementById("firstname")
+ let InputFname=  document.getElementById("input-firstname")
+ let defMname = document.getElementById("middlename")
+ let InputMname=  document.getElementById("input-middlename")
 
-let inputDob = document.getElementById("bdate")
-let dateOfBirth = document.getElementById("dob")
 
-let inputGender = document.getElementById("sex")
-let gender = document.getElementById("gender")
+//
+let defBdate = document.getElementById("dob")
+let InputBdate=  document.getElementById("bdate")
 
-let inputBirthPlace = document.getElementById("bod")
-let pob = document.getElementById("pob")
+let defGender = document.getElementById("gender")
+let InputGender=  document.getElementById("sex")
 
-let address = document.getElementById("def-address")
-let inputAddress = document.getElementById("address")
+let defDob = document.getElementById("pob")
+let InputDob=  document.getElementById("bod")
 
-let blood = document.getElementById("blood")
-let inputBlood = document.getElementById("bloodtype")
+let defAdd = document.getElementById("def-address")
+let InputAdd =  document.getElementById("address")
 
-let eyeColor = document.getElementById("eye")
-let inputEyeColor = document.getElementById("eye-color")
+
+let defBlood = document.getElementById("blood")
+let InputBlood =  document.getElementById("bloodtype")
+
+let defEye = document.getElementById("eye")
+let InputEye =  document.getElementById("eye-color")
+
+let defPicture = document.getElementById("picture")
+let inputPicture = document.getElementById("img")
+
 const submit = document.getElementById("button")
-
 
 submit.addEventListener("click",(event)=>{
     event.preventDefault();
-    fullName.textContent = inputName.value
-    dateOfBirth.textContent = inputDob.value
-    gender.textContent = inputGender.value
-    pob.textContent = inputBirthPlace.value
-    address.textContent = inputAddress.value
-    blood.textContent = inputBlood.value
-    eyeColor.textContent = inputEyeColor.value
+    defSName.textContent = InputSname.value
+    defFname.textContent = InputFname.value
+    defMname.textContent = InputMname.value
+    defBdate.textContent = InputBdate.value  
+    defGender.textContent = InputGender.value
+    defDob.textContent = InputDob.value
+    defAdd.textContent = InputAdd.value
+    defBlood.textContent = InputBlood.value
+    defEye.textContent = InputEye.value
+  
 
 
 })
